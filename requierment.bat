7ae1bed539e92d83a90e0c9b285694e9c6d47cda
pouximixi <104493295+pouximixi0@users.noreply.github.com>
2025-01-16 17:14:17 +0100

    Add files via upload

---
+++ requierment.bat
@@ -0,0 +1,21 @@
+@echo off
+color a
+cls
+@echo off
+color a
+cls
+
+echo 𝕡𝕖𝕣𝕞𝕚𝕤𝕚𝕠𝕟 𝕤𝕔𝕒𝕟𝕟𝕖𝕣
+echo "Vous aite sur le point d'installer les dependances developpeur de permission scanner v1.0 python"
+pause
+
+cls
+python -m pip install --upgrade pip
+cls
+pip install pywin32 
+cls
+pip install tk
+cls
+echo "Tout a ete installe et mis a jour"
+pause
+exit
